e9f455ad706a628524af04f933f2943f7f4bf5bd
srinu8688 <srinu868890@gmail.com>
2025-11-25 22:54:31 +0530

    make the new url to acces backend

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(fileUpload({
 }));
 app.use(
   cors({
-    origin: [process.env.PORTFOLIO_URL,process.env.DASHBOARD_URL,"http://localhost:5173","http://localhost:4000","http://localhost:5174","https://mern-stack-portfolio-backend-mev4.onrender.com"],
+    origin: [process.env.PORTFOLIO_URL,process.env.DASHBOARD_URL,"http://localhost:5173","http://localhost:4000","http://localhost:5174","https://mern-stack-portfolio-backend-mev4.onrender.com","https://personal-mern-stack-portfolio-dashb.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
